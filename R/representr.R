3cfaa6c3f9fe04a98144dc89495416cdbc06427b
Andee Kaplan <andee.kaplan@gmail.com>
2018-06-05 12:13:02 -0400

    add package level documentation file

---
+++ R/representr.R
@@ -0,0 +1,16 @@
+#' representr: A package for creating representative records post-record linkage.
+#'
+#' The representr package provides two types of representative record generation:
+#' prototyping and composite.
+#'
+#' @section Prototyping:
+#'
+#' write about prototyping
+#'
+#' @section Composite record generation:
+#'
+#' write about composite
+#'
+#' @docType package
+#' @name representr
+NULL
