a73acd4bcee7e3ab5d24246f0a880330b3025ebf
Andee Kaplan <andee.kaplan@gmail.com>
2018-06-07 16:59:12 -0400

    pp_weights

--- R/pp_weights.R
+++ R/pp_weights.R
@@ -1,9 +1,9 @@
-#' Create a representative dataset post record-linkage.
+#' Get posterior weights for each record post record-linkage using posterior prototyping.
 #'
 #' @param data A data frame of records to be represented.
-#' @param linkage A numeric vector indicating the cluster ids post-record linkage for each record in \code{data}.
-#' @param rep_method Which method to use for representation. Valid options include "proto_minimax", "proto_random",
-#'     and "composite".
+#' @param posterior_linkage A matrix of size m x n, indicating the posterior cluster ids post-record linkage,
+#'     each row represents the cluster assignment for each record in \code{data} for 1 iteration of the sampler.
+#' @param rep_method Which method to use for representation. Valid options include "proto_minimax" and "proto_random".
 #' @param parallel Logical flag if to use parallel computation or not (via \code{foreach}).
 #' @param cores If specified, the number of cores to use with \code{foreach}.
 #' @param ... Additional parameters sent to cluster representation function. See
@@ -11,41 +11,38 @@
 #' @param scale If "proto_minimax" method is specified, logical flag to indicate if the column-type
 #'     distance function should be scaled so that each distance takes value in [0, 1]. Defaults to
 #'     FALSE.
-#'
 #' @examples
 #'
-#' data("rl_reg1")
+#' data(rl_reg1)
 #'
-#' ## random prototyping
-#' rep_dat_random <- represent(rl_reg1, identity.rl_reg1, "proto_random", id = FALSE)
-#' head(rep_dat_random)
+#' # make a fake posterior distribution for the linkage
+#' m <- 100
+#' n <- nrow(rl_reg1)
+#' post_link <- matrix(sample(seq_len(n), n*m, replace = TRUE), nrow = m)
 #'
-#' ## minimax prototyping
+#' # get the posterior prototyping weights
 #' col_type <- c("string", "string", "numeric", "numeric", "numeric", "categorical", "ordinal",
 #'     "numeric", "numeric")
 #' orders <- list(education = c("Less than a high school diploma", "High school graduates, no college",
 #'     "Some college or associate degree", "Bachelor's degree only", "Advanced degree"))
 #' weights <- c(.25, .25, .05, .05, .1, .15, .05, .05, .05)
-#' rep_dat_minimax <- represent(rl_reg1, identity.rl_reg1, "proto_minimax", id = FALSE,
-#'     distance = dist_col_type, col_type = col_type, weights = weights, orders = orders,
-#'     scale = TRUE)
-#' head(rep_dat_minimax)
+#' pp_weight <- pp_weights(rl_reg1, post_link, "proto_minimax", distance = dist_col_type,
+#'     col_type = col_type, weights = weights, orders = orders, scale = TRUE)
 #'
-#' ## composite prototyping
-#' rep_dat_composite <- represent(rl_reg1, identity.rl_reg1, "composite", col_type = col_type)
-#' head(rep_dat_composite)
+#' # threshold by posterior prototyping weights
+#' head(rl_reg1[pp_weight > 0.5, ])
 #'
 #' @export
-represent <- function(data, linkage, rep_method, parallel = TRUE, cores = NULL, ..., scale = FALSE) {
+pp_weights <- function(data, posterior_linkage, rep_method, parallel = TRUE, cores = NULL, ..., scale = FALSE) {
   ## error handling
   if(!("data.frame" %in% class(data)))
     stop("data must be a data frame.")
-  if(length(linkage) != nrow(data))
-    stop("linkage must have one entry for every record.")
-  if(!is.numeric(linkage))
-    stop("linkage must be numeric.")
-  if(!(rep_method %in% c("proto_minimax", "proto_random", "composite")))
-    stop("Valid options for rep_method include 'proto_minimax', 'proto_random', and 'composite'.")
+  if(ncol(posterior_linkage) != nrow(data))
+    stop("posterior_linkage must have one column for every record.")
+  if(!is.matrix(posterior_linkage))
+    stop("posterior_linkage must be a matrix.")
+  if(!(rep_method %in% c("proto_minimax", "proto_random")))
+    stop("Valid options for rep_method include 'proto_minimax' and 'proto_random'.")
 
   ## check that ... options match method chosen
   args <- list(...)
@@ -55,9 +52,6 @@ represent <- function(data, linkage, rep_method, parallel = TRUE, cores = NULL, 
       stop("Must supply distance function for proto_minimax method. See help('clust_proto_minimax') for more options.")
     else if(class(args[["distance"]]) != "function")
       stop("Must supply distance function for proto_minimax.")
-  } else if(rep_method == "composite") {
-    if(!("col_type" %in% arg_names))
-      stop("Must supply column types for composite method. See help('clust_composite') for more options.")
   }
 
   ## scale option for minimaxm column-type distance
@@ -82,15 +76,6 @@ represent <- function(data, linkage, rep_method, parallel = TRUE, cores = NULL, 
     args$weights <- args$weights*sca/sum(args$weights*sca)
   }
 
-  ## apply representative method to each cluster
-  clusters <- split(data, linkage)
-  k <- length(clusters)
-
-  rep_fun <- switch(rep_method,
-                    "proto_minimax" = clust_proto_minimax,
-                    "proto_random" = clust_proto_random,
-                    "composite" = clust_composite)
-
   `%doit%` <- ifelse(parallel, foreach::`%dopar%`, foreach::`%do%`)
 
   if(parallel) doParallel::registerDoParallel(cores = cores)
@@ -98,11 +83,14 @@ represent <- function(data, linkage, rep_method, parallel = TRUE, cores = NULL, 
   ## register i so that check won't complain
   i <- NULL
 
-  rep_dat <- foreach::foreach(i = 1:k, .combine = rbind) %doit%
-    do.call("rep_fun", c(list(cluster = clusters[[i]]), args)) # complicated because args = ...
+  m <- nrow(posterior_linkage)
+
+  posterior_rep <- foreach::foreach(i = 1:m, .combine = cbind) %doit% {
+    idx <- do.call("represent", c(list(data = data, linkage = posterior_linkage[i,], rep_method = rep_method, scale = FALSE, id = TRUE), args))
+    seq_len(nrow(data)) %in% idx
+  }
 
   if(parallel) doParallel::stopImplicitCluster()
 
-  return(rep_dat)
-
+  rowSums(posterior_rep)/nrow(posterior_linkage)
 }
