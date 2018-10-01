069d80ff9bc01baddbba63a73cf294d046dda1df
Andee Kaplan <andee.kaplan@gmail.com>
2018-10-01 17:14:17 -0400

    clean check

--- inst/extdata/clean_data.R
+++ inst/extdata/clean_data.R
@@ -9,7 +9,7 @@ load("inst/extdata/rl_reg2_raw.RData")
 identity.rl_reg2 <- seq_along(identity)
 identity.rl_reg2[setdiff(identity.rl_reg2, originals_idx)] <- merge(data.frame(id = identity[setdiff(identity.rl_reg2, originals_idx)]), originals_dup, by = "id", all.x = TRUE)$rownum
 rl_reg2 <- data[, -10]
-save(rl_reg2, file = "data/rl_reg5.RData")
+save(rl_reg2, file = "data/rl_reg2.RData")
 save(identity.rl_reg2, file = "data/identity.rl_reg2.RData")
 
 load("inst/extdata/rl_reg5_raw.RData")
